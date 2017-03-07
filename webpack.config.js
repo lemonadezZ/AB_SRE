f4d4bc7ce5107c9d261c4988e4f5949da8e21189
dev-jingdor <945627077@qq.com>
2017-03-06 21:19:17 -0500

    webpack 分离 vendor 添加 代码压缩

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const { resolve } = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: [
+  entry: {
 //	'react-hot-loader/patch',
-	'./src/app.js'
-	],
+	bundle:'./src/app.js',
+	vendor:['react','semantic-ui-react','react-dom','react-router']
+	},
   output: {
     filename: './static/js/bundle.js'
   },
@@ -33,6 +34,14 @@ module.exports = {
     //new webpack.HotModuleReplacementPlugin(),
     // enable HMR globally
     //
+    	new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        },
+        sourceMap: true,//这里的soucemap 不能少，可以在线上生成soucemap文件，便于调试
+        mangle: true
+   	 }),
+       new webpack.optimize.CommonsChunkPlugin({name:'vendor',  filename:'./static/js/vendor.js'}),
        new webpack.NamedModulesPlugin(),
     //         // prints more readable module names in the browser console on HMR updates
        ],
